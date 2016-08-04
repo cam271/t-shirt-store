9c4ab63e4be4793293343b5e49e582a31d819127
Messenger, C <messenger_c@ymail.com>
2016-08-04 00:45:29 -0400

    added assert_select method tests to make sure static_pages had titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,15 +4,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
+    assert_select "title", "T-Shirt-Co"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
+    assert_select "title", "About"
   end
 
   test "should get contact" do
     get contact_path
     assert_response :success
+    assert_select "title", "Contact"
   end
 end
