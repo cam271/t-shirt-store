a532bde43acd7e31a05acb866dcae873ccdf7ed6
Messenger, C <messenger_c@ymail.com>
2016-08-04 00:44:06 -0400

    added logic in def pages_title so that test/controllers/static_pages_controller_test.rb could pass

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
-	def full_title(page_title)
-		base_title = "T-Shirt-Co"
+	# returns the page title or the default title if empty
+	def pages_title(page_title)
+		default_title = "T-Shirt-Co"
 		if page_title.empty?
-			base_title
+			default_title
 		else
-			"#{page_title}"
+			page_title
 		end
 	end
 end
