1ced493a8897f6677bd900447cdcbef75074c9ef
Messenger, C <messenger_c@ymail.com>
2016-08-04 12:59:28 -0400

    added title and get tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -18,4 +18,28 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Contact"
   end
+  
+	test "should get men" do
+    get men_path
+    assert_response :success
+    assert_select "title", "Mens Shirts"
+  end
+  
+	test "should get women" do
+    get women_path
+    assert_response :success
+    assert_select "title", "Womens Shirts"
+  end
+  
+	test "should get boys" do
+    get boys_path
+    assert_response :success
+    assert_select "title", "Boys Shirts"
+  end
+  
+	test "should get girls" do
+    get girls_path
+    assert_response :success
+    assert_select "title", "Girls Shirts"
+  end
 end
