db149b8c0d34eaf795e44f7c5c368253c22166ed
Messenger, C <messenger_c@ymail.com>
2016-08-04 12:48:52 -0400

    added routes for men women boys and girls

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
 	root 										'static_pages#home'
 	get 		'contact' 	=> 	'static_pages#contact'
  	get			'about' 		=>  'static_pages#about' 
-
+	get 		'men'				=>	'static_pages#men'
+	get     'women'     =>  'static_pages#women'
+	get     'boys'      =>  'static_pages#boys'
+	get     'girls'     =>  'static_pages#girls'
 
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
