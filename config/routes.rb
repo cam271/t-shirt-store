38c870de36eddc614d690ba68d509243a1a3cfbb
Messenger, C <messenger_c@ymail.com>
2016-08-03 21:42:27 -0400

    added the routes for the contact and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
-	root 'static_pages#home'
+	root 										'static_pages#home'
+	get 		'contact' 	=> 	'static_pages#contact'
+ 	get			'about' 		=>  'static_pages#about' 
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
