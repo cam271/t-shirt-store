c079d80a5eca0a9801b7e266ca1048090e29bb1a
Messenger, C <messenger_c@ymail.com>
2016-08-04 22:28:14 -0400

    added assert_select tests and modified the counts

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,14 +2,15 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+	# tests the total number of reference links in the site
 	test "sites layout links" do
 		get root_path
 		assert_template 'static_pages/home'
-		assert_select 'a[href=?]', root_path, count:1
-		assert_select 'a[href=?]', men_path, count:1
-		assert_select 'a[href=?]', women_path, count:1
-		assert_select 'a[href=?]', boys_path, count:1
-		assert_select 'a[href=?]', girls_path, count:1
+		assert_select 'a[href=?]', root_path, count:2
+		assert_select 'a[href=?]', men_path, count:2
+		assert_select 'a[href=?]', women_path, count:2
+		assert_select 'a[href=?]', boys_path, count:2
+		assert_select 'a[href=?]', girls_path, count:2
 	end
 
 end
