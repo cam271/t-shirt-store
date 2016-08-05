276bd2060f215b73b2cbb1109482fda8b9cacfd8
Messenger, C <messenger_c@ymail.com>
2016-08-04 23:57:11 -0400

    recreated the products model, because of mispelling of character

--- db/migrate/20160805035549_create_products.rb
+++ db/migrate/20160805035549_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration[5.0]
     create_table :products do |t|
       t.string :category
       t.string :style
-      t.charachter :sex
+      t.character :sex
       t.string :image
 
       t.timestamps
