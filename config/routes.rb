a37bce609d87d9278fb3b1caf0fc396af8f2c10e
Messenger, C <messenger_c@ymail.com>
2016-08-06 12:44:56 -0400

    changed route paths from static_pages to product

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,10 @@ Rails.application.routes.draw do
 	root 										'static_pages#home'
 	get 		'contact' 	=> 	'static_pages#contact'
  	get			'about' 		=>  'static_pages#about' 
-	get 		'men'				=>	'static_pages#men'
-	get     'women'     =>  'static_pages#women'
-	get     'boys'      =>  'static_pages#boys'
-	get     'girls'     =>  'static_pages#girls'
+	get 		'men'				=>	'product#men'
+	get     'women'     =>  'product#women'
+	get     'boys'      =>  'product#boys'
+	get     'girls'     =>  'product#girls'
 
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
