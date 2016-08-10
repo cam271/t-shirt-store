70bd49b296fcf3b748e0949112b93e03a0e9fced
Messenger, C <messenger_c@ymail.com>
2016-08-10 14:21:49 -0400

    fixed typo in reosurces :products

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 	root 										'static_pages#home'
-	resources 							:products, only[:index]
+	resources 							:products, only:[:index]
 	get 		'contact' 	=> 	'static_pages#contact'
  	get			'about' 		=>  'static_pages#about' 
 	
