b59c065e3538e33b3c92d2d93cf8a4a034ae0add
Messenger, C <messenger_c@ymail.com>
2016-08-10 14:13:19 -0400

    removed links to old pages {men, women, etc..} and changed them to link to one index.html.erb page

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,10 @@
 Rails.application.routes.draw do
-  
-  get 'product/men'
-
-  get 'product/women'
-
-  get 'product/boys'
-
-  get 'product/girls'
 
 	root 										'static_pages#home'
+	resources 							:products, only[:index]
 	get 		'contact' 	=> 	'static_pages#contact'
  	get			'about' 		=>  'static_pages#about' 
-	get 		'men'				=>	'product#men'
-	get     'women'     =>  'product#women'
-	get     'boys'      =>  'product#boys'
-	get     'girls'     =>  'product#girls'
+	
 
-	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
