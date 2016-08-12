0c762ce6d22233ea45e92bf423de7ff455e837d6
Messenger, C <messenger_c@ymail.com>
2016-08-11 20:24:20 -0400

    added cart resource to config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
 
+	# this was created with the controller
   get 'carts/show'
-
+	# home page
 	root 										'static_pages#home'
+	# tshirts display pages
 	resources 							:products, only:[:index]
+	# shopping cart
+	resources							:carts, only [:show] do
+		put 'add/:tshirt_id', to: 'carts#add', as: :add_to
+		put 'remove/:tshirt_id', to: 'carts#remove', as: :remove_from
+	end
+	# about and contact pages
 	get 		'contact' 	=> 	'static_pages#contact'
  	get			'about' 		=>  'static_pages#about' 
 	
