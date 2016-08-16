ecb0611cb2d93187375c6fa23c842422a4586e00
Messenger, C <messenger_c@ymail.com>
2016-08-16 01:29:02 -0400

    added to products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160815233927) do
+ActiveRecord::Schema.define(version: 20160816041513) do
 
   create_table "order_items", force: :cascade do |t|
     t.integer  "product_id"
