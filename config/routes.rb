9e7e9fd627fe4b80b1c7d342dbb27ac8a4ab96fa
Messenger, C <messenger_c@ymail.com>
2016-08-17 17:59:08 -0400

    commented out the auto generated routes and inserted my onw resources for them

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,26 @@
 Rails.application.routes.draw do
 
-  get 'order_items/create'
+	# these were created with controller generations
+ # get 'order_items/create'
 
-  get 'order_items/update'
+ # get 'order_items/update'
 
-  get 'order_items/destroy'
+ # get 'order_items/destroy'
 
-	# this was created with the controller
-  get 'carts/show'
+ # get 'carts/show'
 	
 	# home page
-	root 										'static_pages#home'
+	root 'static_pages#home'
 	
-	# tshirts display pages
-	resources 							:products, only:[:index]
+	# tshirts display products
+	resources :products, only: [:index]
 	
-	# shopping cart
-	resources							:carts, only:[:show] do
-		put 'add/:tshirt_id', to: 'carts#add', as: :add_to
-		put 'remove/:tshirt_id', to: 'carts#remove', as: :remove_from
-	end
+	# shopping cart display items in basket
+	resource :cart, only: [:show]
 	
+	# modify items in for the basket
+  resources :order_items, only: [:create, :update, :destroy]
+
 	# about and contact pages
 	get 		'contact' 	=> 	'static_pages#contact'
  	get			'about' 		=>  'static_pages#about' 
