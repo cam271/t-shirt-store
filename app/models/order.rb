e9128e560a189dd97c28388391a61ab7b643bf1c
Messenger, C <messenger_c@ymail.com>
2016-08-17 14:54:53 -0400

    first edits

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,26 @@
 class Order < ApplicationRecord
   belongs_to :order_status
+	has_many :order_items
+	before_create :set_order_status
+	before_save :update_subtotal
+
+	# gets called by before_create method to either set subtotal if there are items or 0 if their are not
+ 	def subtotal
+    order_items.collect { |oi| oi.valid? ? (oi.quantity * oi.unit_price) : 0 }.sum
+  end
+	
+
+	#private methods
+	private
+
+	#sets order status to equal in progress
+  def set_order_status
+    self.order_status_id = 1
+  end
+	
+	#sets the tables subtotal column to current total
+  def update_subtotal
+    self[:subtotal] = subtotal
+  end
+
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,45 @@
 class OrderItem < ApplicationRecord
   belongs_to :product
   belongs_to :order
+
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validate :product_present
+  validate :order_present
+
+  before_save :finalize
+
+  def unit_price
+    if persisted?
+      self[:unit_price]
+    else
+      product.price
+    end
+  end
+
+  def total_price
+    unit_price * quantity
+  end
+
+	# private methoda
+	private
+	# making sure the foreign key which this belongs to is not nil
+  def product_present
+    if product.nil?
+      errors.add(:product, "is not valid or is not active.")
+    end
+  end
+
+	# making sure the foreign key which this belongs to is not nil
+  def order_present
+    if order.nil?
+      errors.add(:order, "is not a valid order.")
+    end
+  end
+
+	# sets the tables columns to their final unit and total price (tax)
+  def finalize
+    self[:unit_price] = unit_price
+    self[:total_price] = quantity * self[:unit_price]
+	end
+  
 end
