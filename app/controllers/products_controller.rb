ed4f75c75d8bbaf75ca39f689dadad312ea87860
Messenger, C <messenger_c@ymail.com>
2016-08-22 17:41:22 -0400

    add to index method order_item variable as a new instance of our OrderItem model for use in our forms

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,9 @@
 class ProductsController < ApplicationController
-	
+
+	# finds all products so that they can be displayed, and creates a new instance of our OrderItem model for use in our forms.
 	def index
   	@products = Product.where(category: params[:category])
+    @order_item = current_order.order_items.new
   end
 
 end
