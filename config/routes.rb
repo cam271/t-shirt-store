c2843d49a96e1d7e66a3b1e3dc99fab6f849224b
Messenger, C <messenger_c@ymail.com>
2016-08-24 00:20:31 -0400

    commented out the auto gen routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
-	# these were created with controller generations
- # get 'order_items/create'
-
- # get 'order_items/update'
-
- # get 'order_items/destroy'
-
- # get 'carts/show'
+#	# these were created with controller generations
+#   get 'order_items/create'
+#
+#   get 'order_items/update'
+#
+#   get 'order_items/destroy'
+#
+#   get 'carts/show'
 	
 	# home page
 	root 'static_pages#home'
