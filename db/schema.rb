4d53e0140deda456964c762fff22949afbc58817
Messenger, C <messenger_c@ymail.com>
2016-09-05 01:16:08 -0400

    added index to users email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816054618) do
+ActiveRecord::Schema.define(version: 20160901044944) do
 
   create_table "order_items", force: :cascade do |t|
     t.integer  "product_id"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20160816054618) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
