1b9ecd84061dbecd081c74874432957371ece119
Messenger, C <messenger_c@ymail.com>
2016-09-05 01:12:16 -0400

    added data to seed order status

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,9 @@ require 'csv'
 CSV.foreach(Rails.root.join("db/seeds_data/t-shirts.csv"), headers: true) do |row|
 	Product.find_or_create_by(category: row[0], image: row[1], price: row[2], active: row[3])
 end  
+
+OrderStatus.delete_all
+OrderStatus.create! id: 1, name: "In Progress"
+OrderStatus.create! id: 2, name: "Placed"
+OrderStatus.create! id: 3, name: "Shipped"
+OrderStatus.create! id: 4, name: "Cancelled"
