417622e4af4790e959ee17b9d8cdd9ce4bdc9e0b
Messenger, C <messenger_c@ymail.com>
2017-07-24 18:59:12 -0400

    Deleted all tests files in order to use rspec

--- test/controllers/order_items_controller_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class OrderItemsControllerTest < ActionDispatch::IntegrationTest
-end
--- test/controllers/products_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ProductsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get products_url(category: "men")
-    assert_response :success
-  end
-
-end
