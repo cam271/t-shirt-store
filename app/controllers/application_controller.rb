b878b0f48761be3787100abb58191ef3a66892ed
Messenger, C <messenger_c@ymail.com>
2017-07-27 22:27:19 -0400

    Devise related data in navbar working when user is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_order
+  # uncoment this when devise error goes away: before_action :authenticate_user!
 
   # helper method to create order throughout the seesion or new order
   def current_order
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+  
+  # Added after installing shine for users
+  #protect_from_forgery with: :exception
+  #before_action :authenticate_user!
+
 	# returns the page title or the default title if empty
 	def pages_title(page_title)
 		default_title = "T-Shirt-Co"
